3689981acbfa76d798089183612750c694029612
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2025-07-11 08:03:40 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "mongodb+srv://tharusha:Tharu22@cluster0.fxqbk.mongodb.net/" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const DATABASE = "postgres://isurucm1_user:NveZZiL2Dpb69vTeYGpAdFUyk6l63AG9@dpg-cl88be76e7vc73a3hbg0-a.frankfurt-postgres.render.com/isurucm1" // "mongodb+srv://tharusha:Tharu22@cluster0.fxqbk.mongodb.net/" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94711450710" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
 const SESSION_ID = "PRABATH-MD~PhlqPEmDYYsGKSY" //"PRABATH-MD~AucADIyY#VD-PKQpL0cOgYFl9-oRlmcduUl9qPvZUrVlKnLgFPo0" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
