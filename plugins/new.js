d6e13956b92c2143559edd924efea4abeaa8fe37
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2025-07-13 21:40:35 +0530

    Update new.js

--- plugins/new.js
+++ plugins/new.js
@@ -26,7 +26,7 @@ cmd({
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-    const kav = await getBuffer("https://mmg.whatsapp.net/v/t62.43144-24/10000000_2572528633088964_4003891503007524239_n.enc?ccb=11-4&oh=01_Q5Aa2AH7XKTlqp02AVKIi1M3vQHQhoQBdK7aYY84lOklAIIKJA&oe=6899DB27&_nc_sid=5e03e0&mms3=true")
+    const kav = "https://mmg.whatsapp.net/v/t62.43144-24/10000000_2572528633088964_4003891503007524239_n.enc?ccb=11-4&oh=01_Q5Aa2AH7XKTlqp02AVKIi1M3vQHQhoQBdK7aYY84lOklAIIKJA&oe=6899DB27&_nc_sid=5e03e0&mms3=true"
 const videoPayload = {
       url: kav
     };
