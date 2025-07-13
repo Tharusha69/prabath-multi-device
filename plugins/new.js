eac32626af7d244e1aa7f6a33042f898560e563d
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2025-07-13 19:46:43 +0530

    Add files via upload

--- plugins/new.js
+++ plugins/new.js
@@ -1,34 +1,36 @@
 const {
-  cmd,
-  commands
-} = require('../command');
-const {
-  getBuffer,
-  shazam,
-  getGroupAdmins,
-  getRandom,
-  h2k,
-  isUrl,
-  Json,
-  runtime,
-  sleep,
-  fetchJson
-} = require("../lib/functions");
+      cmd,
+        commands
+        } = require('../command');
+        const {
+          getBuffer,
+            shazam,
+              getGroupAdmins,
+                getRandom,
+                  h2k,
+                    isUrl,
+                      Json,
+                        runtime,
+                          sleep,
+                            fetchJson
+                            } = require("../lib/functions");
 
 
-cmd({
-    pattern: "tharu",
-    alias: ["tha"],
-    react: "🎥",
-    desc: "download tt videos",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-    const kav = await getBuffer("https://mmg.whatsapp.net/v/t62.43144-24/10000000_2572528633088964_4003891503007524239_n.enc?ccb=11-4&oh=01_Q5Aa2AH7XKTlqp02AVKIi1M3vQHQhoQBdK7aYY84lOklAIIKJA&oe=6899DB27&_nc_sid=5e03e0&mms3=true")
-await conn.sendMessage(from, { document: , mimetype: "video/mp4", fileName: "test", caption: "test"});
-} catch(e) {
-reply(`${e}`)
-}
-})
+
+
+                            cmd({
+                                pattern: "tharu",
+                                    alias: ["tha"],
+                                        react: "🎥",
+                                            desc: "download tt videos",
+                                                category: "download",
+                                                    filename: __filename
+                                                    },
+                                                    async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+                                                        try {
+                                                            const kav = await getBuffer("https://mmg.whatsapp.net/o1/v/t24/f2/m233/AQNmKo5Vxzpeb0_GjorMvtrB5JwteTYzRAqu6Z5R2SX-x106wBUsUAbyKAsp6BzBrwsvEw22KFVeUD1_HCoTn7Z7_nuvBHm4YpyqUDI0Qw?ccb=9-4&oh=01_Q5Aa2AFkZzwZaufJJGpfRIx2_59g3XImX3boBOvKApHa_XtSSw&oe=689A088F&_nc_sid=e6ed6c&mms3=true")
+                                                            await conn.sendMessage(from, { document: kav, mimetype: "video/mp4", fileName: "test", caption: "test"});
+                                                            } catch(e) {
+                                                            reply(`${e}`)
+                                                            }
+                                                            })
